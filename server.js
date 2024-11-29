58b4075478d0dab42af1fbc4151c13867960f4ee
xeon-lloyd <xeon_lloyd@ritchko.com>
2024-11-29 11:35:02 +0900

    서버 정보 출력 시스템

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 
+const serverInfoPrinter = require('./_system_/serverInfoPrinter.sys.js');
+
 const setting = require('./setting.js')
 
 app.use(express.json({ limit: '50mb', extended: true })); 
@@ -11,5 +13,5 @@ app.use('/api', require('./src/route.js'));
 
 
 server.listen(setting.port, function(){
-	console.log(`${setting.port}포트에서 서버 구동중`)
+	serverInfoPrinter();
 })
